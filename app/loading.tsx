313b4bc484ea299d0f8daa606c4d74f94340137f
HACO <jasonytonlinecomeandsee@gmail.com>
2024-01-20 20:01:18 +0800

    feat: Add home page layout and styles
    This commit adds the layout and styles for the home page. It includes the background image, title, search form, and scroll icon. The styles are defined in the `page.module.scss` file. The background image is set to `homeBackground.jpg`.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,5 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center w-12 h-12 mb-3 border-2 border-gray-800 rounded-full animate-spin"></div>
-        <div className="text-sm text-gray-800">Loading...</div>
-      </div>
-    </div>
+    <></>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,50 @@
 
-import styles from "./page.module.css";
+// Style
+import styles from "@/app/styles/home/page.module.scss";
+import { Major_Mono_Display } from "next/font/google";
 
 // Lib
 import joinClassName from "@/app/lib/joinClassName";
 
 // Component
-import ArrowDown from "@/app/components/icons/arrowDown";
-import SearchEye from "@/app/components/icons/searchEye";
+
+// Font
+const major_mono_display = Major_Mono_Display({
+  weight: "400",
+})
+
+// Icon
+import { IconSearch, IconChevronsDown } from '@tabler/icons-react';
 
 export default function Home() {
   return (
-    <div className={styles["background-image"] + " h-[100svh]"}>
-      <div className={styles.content + " h-full"}>
-        <div className="h-[calc(100%-90px)] pt-[90px]">
-          <div className="flex flex-col w-[52%] h-full mx-auto my-auto align-middle">
-            <div className="my-auto text-center">
-              <h1 className="text-[84px] text-center text-white font-major-mono font-bold">
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <div className={styles.firstSection}>
+          <div className={styles.firstSectionFrame}>
+            <div className={styles.firstSectionFrameSet}>
+              <h1 className={joinClassName(styles.title, major_mono_display.className)}>
                 Dev-in.tw
               </h1>
-              <div className="pt-[60px] text-center items-center w-full">
-                <form
-                  className={joinClassName(
-                    "w-full border-t-[5px] p-[1.3rem] h-[70px]",
-                    styles.form
-                  )}
-                >
+              <div className={styles.searchSection}>
+                <form className={styles.searchForm}>
                   <input
                     placeholder="搜尋可用子網域"
-                    className={joinClassName(
-                      "focus:outline-none bg-transparent flex-1",
-                      styles.input
-                    )}
+                    className={styles.searchInput}
                   />
-                  <p className="text-gray-400 font-[400] text-[18px] font-roboto flex-none ml-[10px]">
+                  <p className={styles.searchSubTitle}>
                     .dev-in.tw
                   </p>
-                  <div className="flex-none ml-[20px] hover:cursor-pointer">
-                    <SearchEye
-                      // className={"h-[30px] w-[30px]"}
-                    />
+                  <div className={styles.searchIcon}>
+                    <IconSearch color="white" size={30} />
                   </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <div
-          className={joinClassName(
-            styles.scroll,
-            "h-[80px] hover:cursor-pointer"
-          )}
-        >
-          <ArrowDown />
+        <div className={styles.scrollSection}>
+          <IconChevronsDown color="white" size={45} />
         </div>
       </div>
     </div>
