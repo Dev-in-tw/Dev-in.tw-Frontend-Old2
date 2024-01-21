ea6d89317e796cbd148a04afc7b65437ede7721e
HACO <jasonytonlinecomeandsee@gmail.com>
2024-01-22 03:59:53 +0800

    feat: Add Discord authentication support
    This commit adds support for Discord authentication using the NextAuth library. It includes the following changes:
    
    - Added .env.local.example file with required environment variables
    - Added route file for handling authentication requests
    - Updated navbar component to include a sign-in button
    - Updated package.json to include the next-auth library
    - Updated pnpm-lock.yaml with the new dependencies
    
    These changes will trigger a release bumping the MINOR version.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 // Module
 import Link from "next/link";
 import Image from "next/image";
 
 // Style
 import styles from "@/app/styles/navbar.module.scss";
+
+// module
+import { signIn } from "next-auth/react";
 
 export default function Navbar() {
   return (
@@ -12,10 +17,10 @@ export default function Navbar() {
         <Image src="/images/logo.png" height={45} width={45} alt="logo" />
       </Link>
       <div className={styles.frame}>
-        <div className={styles.signInButton}>
+        <button className={styles.signInButton} onClick={() => signIn("discord")}>
           <p className={styles.signInButtonText}>登入 / 註冊</p>
-        </div>
-        <div className={styles.plusButton}>
+        </button>
+        <button className={styles.plusButton}>
           <svg
             viewBox="0 0 30 30"
             xmlns="http://www.w3.org/2000/svg"
@@ -25,8 +30,8 @@ export default function Navbar() {
               <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z" />
             </g>
           </svg>
-        </div>
+        </button>
       </div>
-    </div>
+    </div >
   );
 }
