75056897f7f9b29230dba5b4b79bbeec327da2ef
HACO <haco@lazco.dev>
2024-01-31 10:38:55 +0800

    feat: Add signIn callback to NextAuth route
    This commit adds a signIn callback to the NextAuth route in order to log user, account, and profile information. It also includes logic to determine if the user is allowed to sign in and returns the appropriate response.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,9 +5,21 @@ const handler = NextAuth({
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!
-    })
-  ]
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // eslint-disable-next-line no-console
+      console.log(user, account, profile);
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return "/";
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
