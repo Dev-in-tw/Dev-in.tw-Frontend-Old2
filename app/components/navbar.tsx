ea2f4e60386479e240d16f94d071a7b32e658df3
HACO <haco@lazco.dev>
2024-02-02 02:43:17 +0800

    style: Fix formatting in navbar module
    Fixed some formatting issues in the navbar module, including indentation and spacing.
    This commit does not introduce any new features or fix any bugs.
    It only improves the code's readability and maintainability.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,11 +8,12 @@ import Image from "next/image";
 import styles from "@/app/styles/navbar.module.scss";
 
 // module
-import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <>
@@ -23,7 +24,7 @@ export default function Navbar() {
         <div className={styles.frame}>
           <button
             className={styles.signInButton}
-            onClick={() => signIn("discord")}
+            onClick={() => router.push("/api/auth/login")}
           >
             <p className={styles.signInButtonText}>登入 / 註冊</p>
           </button>
@@ -32,33 +33,64 @@ export default function Navbar() {
             onClick={() => {
               setMenuOpen(!menuOpen);
             }}
+            onMouseEnter={element => {
+              if (menuOpen) {
+                const svgElement = element.currentTarget.querySelector("svg");
+                if (svgElement) {
+                  svgElement.style.transform = "rotate(-135deg)";
+                }
+              }
+            }}
+            onMouseLeave={element => {
+              if (menuOpen) {
+                const svgElement = element.currentTarget.querySelector("svg");
+                if (svgElement) {
+                  svgElement.style.transform = "rotate(-315deg)";
+                }
+              }
+            }}
           >
+            {menuOpen ? (
+              <div className={styles.plusButtonLeftBottomTriangle} />
+            ) : (
+              <div className={styles.plusButtonRightTopTriangle} />
+            )}
             <svg
               viewBox="0 0 30 30"
               xmlns="http://www.w3.org/2000/svg"
               className={styles.plusIcon}
+              style={{
+                transform: menuOpen ? "rotate(-135deg)" : "",
+              }}
             >
               <g mask="url(#mask0_21_345)">
                 <path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z" />
               </g>
             </svg>
           </button>
+
+          <div
+            className={styles.menuOverlay}
+            style={{
+              backgroundColor: menuOpen
+                ? "rgba(0, 0, 0, 0.5)"
+                : "rgba(0, 0, 0, 0)",
+              pointerEvents: menuOpen ? "auto" : "none",
+              backdropFilter: menuOpen ? "blur(10px)" : "blur(0px)",
+            }}
+            onClick={() => setMenuOpen(false)}
+          >
+            <div
+              className={styles.menuFrame}
+              style={{
+                transform: menuOpen ? "" : "translateY(-100%)",
+              }}
+              onClick={event => event.stopPropagation()}
+            >
+              Menu Content
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        className={styles.menuOverlay}
-        style={{
-          display: menuOpen ? "" : "none",
-        }}
-      ></div>
-      <div
-        className={styles.menuFrame}
-        style={{
-          transform: menuOpen ? "" : "scaleY(0)",
-        }}
-      >
-        Menu Content
-        <button onClick={() => setMenuOpen(!menuOpen)}>Close</button>
       </div>
     </>
   );
