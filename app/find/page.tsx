0cb596dc7e13e983a960a60c3a526ccb410bd44d
HACO <haco.tw@gmail.com>
2024-02-06 21:43:44 +0800

    feat: Add login page and styling changes
    - a new login page component
    - Redirect users the login page when accessed
    - Updated styling for the find page and navbar

--- app/find/page.tsx
+++ app/find/page.tsx
@@ -10,5 +10,12 @@ export default function Find() {
   const searchParams = useSearchParams();
   const sub = searchParams.get("sub");
 
-  return sub;
+  return (
+    <div className={styles.main}>
+      <div className={styles.resultFrame}>
+        <div className={styles.resultBox}>Domain 1</div>
+        <div className={styles.resultBox}>Domain 2</div>
+      </div>
+    </div>
+  );
 }
